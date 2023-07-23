e233ac6f04f038da710d2ae8d014ba1428277910
arteraz <64402118+arteraz@users.noreply.github.com>
2023-07-23 14:02:12 +0700

    copy Project to Git

---
+++ models/response/accountResponse.ts
@@ -0,0 +1,10 @@
+export default interface AccountResponse {
+  id: string;
+  ID_std: string;
+  name: string;
+  lastName: string;
+  address: string;
+  phoneNo: number;
+  GPA: number;
+  major: string;
+}
